02db89124982cd3d54f1ca883b70f62bde40eec7
Aakash Rajput <aakash.ar1213@gmail.com>
2024-05-16 22:49:50 +0530

    Added additional Message

--- lib/utils.ts
+++ lib/utils.ts
@@ -198,5 +198,7 @@ export const getTransactionStatus = (date: Date) => {
 
 export const authFormSchema = z.object({
   email: z.string().email(),
-    password: z.string().min(8),
+    password: z.string().min(8, {
+      message:"Password should be 8 characters long"
+    }),
 })
